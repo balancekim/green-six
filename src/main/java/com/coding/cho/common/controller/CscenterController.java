a19312519cbbf0e015861293da8a462fe6ae4ce2
Administrator <Administrator@304-27>
2023-07-26 13:51:52 +0900

    0726 SM

--- src/main/java/com/coding/cho/common/controller/CscenterController.java
+++ src/main/java/com/coding/cho/common/controller/CscenterController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coding.cho.common.service.CscenterService;
@@ -39,6 +40,5 @@ public class CscenterController {
 	public String write() {
 		return "user/cscenter/faq/write";
 	}
-	
 	
 }
