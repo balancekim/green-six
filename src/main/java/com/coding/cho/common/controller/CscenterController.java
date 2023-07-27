03b7cfa9b68f75867093bebd49de2bc3fe62eed4
Administrator <Administrator@304-27>
2023-07-27 15:18:51 +0900

    0727 sm

--- src/main/java/com/coding/cho/common/controller/CscenterController.java
+++ src/main/java/com/coding/cho/common/controller/CscenterController.java
@@ -3,12 +3,14 @@ package com.coding.cho.common.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coding.cho.common.domain.dto.FaqBoardSaveDTO;
+import com.coding.cho.common.domain.dto.cscenter.FaqBoardUpdateDTO;
 import com.coding.cho.common.service.CscenterService;
 import com.coding.cho.common.service.FaqBoardService;
 
@@ -55,4 +57,37 @@ public class CscenterController {
 		fBService.save(authentication.getName(), dto);
 		return "redirect:/common/cscenter";
 	}
+	
+	@GetMapping("/faq/board/detail/{no}")
+	public String boardDetail(Model model, @PathVariable("no") long no) {
+		fBService.faqBoardDetail(no, model);
+		
+		return "user/cscenter/faq/boardDetail";
+	}
+	
+	@GetMapping("/faq/board/modify/{no}")
+	public String boardModify(Model model, @PathVariable("no") long no) {
+		fBService.faqBoardModify(no, model);
+		
+		return "user/cscenter/faq/boardModify";
+	}
+	
+	@GetMapping("/faq/board/delete/{no}")
+	public String boardDelete(Authentication authentication,@PathVariable("no") long no) {
+		boolean isOwner = fBService.isOwner(authentication.getName(), no);
+		if(isOwner) {
+			fBService.faqBoardDelete(no);
+			return "user/cscenter/faq/list";
+		} else {
+			return "user/cscenter/faq/list";
+		}
+	}
+	
+	@PostMapping("/faq/board/update/{no}")
+	public String boardUpdate(FaqBoardUpdateDTO dto, @PathVariable("no") long no) {
+		fBService.faqBoardupdate(dto, no);
+		
+		return "redirect:/faq/board/detail/{no}";
+	}
+	
 }
