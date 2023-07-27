01f1c66968ccf95a53d8336b6696a2782a579fe7
Administrator <Administrator@304-10>
2023-07-27 11:23:24 +0900

    cnrk

--- src/main/java/com/coding/cho/goods/GoodsEntityRepository.java
+++ src/main/java/com/coding/cho/goods/GoodsEntityRepository.java
@@ -1,8 +1,11 @@
 package com.coding.cho.goods;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coding.cho.goods.dto.GoodsSaveDTO;
+
 
 public interface GoodsEntityRepository extends JpaRepository<GoodsEntity, Long>{
 
