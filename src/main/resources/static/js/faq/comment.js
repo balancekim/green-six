4a71ed5c5d4f4967a6326ac0c668eb642d45ebbb
Administrator <Administrator@304-27>
2023-07-27 17:51:54 +0900

    0727 SM

---
+++ src/main/resources/static/js/faq/comment.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+$(function(){
+	$('.edit-btn').click(editClick);
+	$('.edit-cancel').click(cancelClick);
+})
+
+function editClick(){
+	var udwrap=$(this).parents(".udwrap");
+	$(".content").hide();
+	udwrap.find(".update").hide();
+	udwrap.find('.form-update').show();
+}
+
+function cancelClick(){
+	var udwrap=$(this).parents(".udwrap");
+	udwrap.find(".update").show();
+	udwrap.find('.form-update').hide();
+}
